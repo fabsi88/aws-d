0a23cd99af4973f1c826e98f30a3ddffd70be630
Fabian Wallentowitz <fabian@wallentowitz.de>
2014-03-16 19:59:11 +0100

    Added properties

--- source/common/aws/request/CRequest.d
+++ source/common/aws/request/CRequest.d
@@ -11,6 +11,23 @@ private:
 	string[] m_params;
 
 public:
+
+	@property
+	{
+		///
+		string baseUrl() const { return m_baseUrl; }
+		///
+		void baseUrl(string _baseUrl) { m_baseUrl = _baseUrl; }
+		///
+		string method() const { return m_method; }
+		///
+		void method(string _method) { m_method = _method; }
+		///
+		string[] params()  { return m_params; }
+		///
+		void params(string[] _params) { m_params = _params; }
+	}
+
 	///
 	this()
 	{
